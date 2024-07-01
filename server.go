cfcc178bfe55ef2a642be332b02dad418617d8a3
Sean <psean6@vt.edu>
2024-07-01 01:51:35 -0400

    simplified routing

--- server.go
+++ server.go
@@ -4,28 +4,79 @@ package main
 
 import (
 	"context"
+	"embed"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
+	"runtime"
+	"strconv"
 	"sync"
 	"time"
-
-	"server/pkg/router"
 )
+
+// content holds static web server content.
+//
+//go:embed app/index.html
+//go:embed app/http_error/404.html
+var content embed.FS
 
 type Config struct {
 	host string
 	port string
 }
 
+func addRoutes(
+	mux *http.ServeMux,
+) {
+	mux.HandleFunc("/", handleHTML)
+	// mux.HandleFunc("/api/v1/stats", handleServerStats)
+}
+
+func handleHTML(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	var data []byte
+	switch r.URL.Path {
+	// case "<URL/ROUTE>":
+	// 	data = content.ReadFile("<FilePath>"")
+	case "/":
+		data, _ = content.ReadFile("app/index.html")
+	default:
+		data, _ = content.ReadFile("app/http_error/404.html")
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(data))
+}
+
+func handleServerStats(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	var data []byte
+
+	cpuCores := "Number of CPU's: " + strconv.Itoa(runtime.NumCPU()) + "\n"
+	goRoutines := "Number of Go Routine's: " + strconv.Itoa(runtime.NumGoroutine()) + "\n"
+	var mem runtime.MemStats
+	runtime.ReadMemStats(&mem)
+
+	mallocs := "Memory Allocated: " + strconv.Itoa(int(mem.Mallocs)) + "B\n"
+	heapAlloc := "Heap Allocated: " + strconv.Itoa(int(mem.HeapAlloc)) + "B\n"
+	heapInUse := "Heap In Use: " + strconv.Itoa(int(mem.HeapInuse)) + "B\n"
+
+	data = []byte(cpuCores + goRoutines + mallocs + heapAlloc + heapInUse)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(data))
+}
+
 func NewServer(
 	config Config,
 ) http.Handler {
 	mux := http.NewServeMux()
-	router.AddRoutes(mux)
+	addRoutes(mux)
 	var handler http.Handler = mux
 	// Add middleware as follows
 	// handler = someMiddleware(handler)
